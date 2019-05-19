318589bf53a95145cd59c746bdae678f94d0b6b8
Mattia Panzeri <mattia.panzeri93@gmail.com>
2019-05-19 19:28:39 +0200

    chore(Demo): Update demo code to showcase more KeycloakProvider props

--- src/demo/App.js
+++ src/demo/App.js
@@ -7,10 +7,40 @@ import { AppRouter } from './routes';
 
 const keycloak = new Keycloak();
 
-const App = () => (
-  <KeycloakProvider keycloak={keycloak}>
-    <AppRouter />
-  </KeycloakProvider>
-);
+class PersistedApp extends React.PureComponent {
+  constructor(props) {
+    super(props);
 
-export default App;
+    this.tokens = JSON.parse(localStorage.getItem('kcTokens') || '{}');
+  }
+
+  onKeycloakEvent = (event, error) => {
+    console.log('onKeycloakEvent', event, error);
+    if (event === 'onAuthLogout') {
+      localStorage.removeItem('kcTokens');
+    }
+  }
+
+  onKeycloakTokens = tokens => {
+    console.log({ tokens });
+    localStorage.setItem('kcTokens', JSON.stringify(tokens));
+  }
+
+  render() {
+    return (
+      <KeycloakProvider
+        keycloak={keycloak}
+        initConfig={{
+          onLoad: 'check-sso',
+          ...this.tokens,
+        }}
+        onEvent={this.onKeycloakEvent}
+        onTokens={this.onKeycloakTokens}
+      >
+        <AppRouter />
+      </KeycloakProvider>
+    );
+  }
+}
+
+export default PersistedApp;
