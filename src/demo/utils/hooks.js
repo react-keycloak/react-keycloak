9960ec368709ae898b1673fa0bd51f1478cf9477
Mattia Panzeri <mattia.panzeri93@gmail.com>
2019-08-09 12:00:38 +0200

    chore(Demo): Improve demo app code

---
+++ src/demo/utils/hooks.js
@@ -0,0 +1,26 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+import { useKeycloak } from '../../lib';
+
+export const useAxios = baseURL => {
+  const [keycloak, initialized] = useKeycloak();
+  const [axiosInstance, setAxiosInstance] = useState({});
+
+  useEffect(() => {
+    const instance = axios.create({
+      baseURL,
+      headers: {
+        Authorization: initialized ? `Bearer ${keycloak.token}` : undefined,
+      },
+    });
+
+    setAxiosInstance({ instance });
+
+    return () => {
+      setAxiosInstance({});
+    };
+  }, [baseURL, initialized, keycloak, keycloak.token]);
+
+  return axiosInstance.instance;
+};
