9b6f69a67befd48a952bb5658489eb012bbcd4f4
Mattia Panzeri <mattia.panzeri93@gmail.com>
2019-12-16 14:26:33 +0100

    feat(Examples): Port 'react-navi' example to monorepo

---
+++ examples/react-navi/src/routes/utils.js
@@ -0,0 +1,9 @@
+import { map, redirect } from 'navi';
+
+export function withAuthentication(matcher) {
+  return map((request, context) =>
+    context.isAuthenticated
+      ? matcher
+      : redirect('/login?redirectTo=' + encodeURIComponent(request.mountpath + request.search))
+  );
+}
