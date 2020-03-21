d9977049301d6a78857e4e39da4b2b7265f13084
Mattia Panzeri <mattia.panzeri93@gmail.com>
2020-03-21 15:28:04 +0100

    chore(nextjs): Update build setup

--- packages/nextjs/.babelrc.js
+++ packages/nextjs/.babelrc.js
@@ -21,6 +21,12 @@ module.exports = {
     '@babel/plugin-proposal-nullish-coalescing-operator',
     // don't use `loose` mode here - need to copy symbols when spreading
     '@babel/proposal-object-rest-spread',
+    [
+      'babel-plugin-transform-react-remove-prop-types',
+      {
+        mode: 'unsafe-wrap'
+      }
+    ],
     NODE_ENV === 'test' && '@babel/transform-modules-commonjs'
   ].filter(Boolean)
 }
