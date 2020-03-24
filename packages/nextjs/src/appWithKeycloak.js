a2b26a4b945d824ad71bf35239bbe71878120ad2
mohux <57658626+mohux@users.noreply.github.com>
2020-03-24 14:19:25 +0300

    Update appWithKeycloak.js

--- packages/nextjs/src/appWithKeycloak.js
+++ packages/nextjs/src/appWithKeycloak.js
@@ -1,124 +1,120 @@
-import React from "react";
-import hoistStatics from "hoist-non-react-statics";
-import PropTypes from "prop-types";
+import React from 'react'
+import hoistStatics from 'hoist-non-react-statics'
+import PropTypes from 'prop-types'
 
-import { getKeycloakInstance } from "./internals/keycloak";
-import KeycloakProvider from "./internals/keycloakProvider";
-import ServerProvider from "./internals/serverProvider";
-import { checkIfUserAuthenticated, setCookie, removeCookie } from "./internals/utils";
-import { TOKEN_COOKIES_AGE } from "~/env";
-import jsCookies from "js-cookies";
-import { login,logout } from "~/components/hoc/withAuthSync";
+import { getKeycloakInstance } from './internals/keycloak'
+import KeycloakProvider from './internals/keycloakProvider'
+import ServerProvider from './internals/serverProvider'
+import { checkIfUserAuthenticated, setCookie } from './internals/utils'
 
 const appWithKeycloak = (
   keycloakInitOptions,
   providerProps = {}
 ) => NextApp => {
-  const keycloak = getKeycloakInstance(keycloakInitOptions);
+  const keycloak = getKeycloakInstance(keycloakInitOptions)
 
   async function getComponentInitialProps({ Component, ctx }) {
-    return Component.getInitialProps
-      ? await Component.getInitialProps(ctx)
-      : {};
+    return Component.getInitialProps ? await Component.getInitialProps(ctx) : {}
   }
 
   async function getKeycloakInitialProps(appContext) {
     return NextApp.getKeycloakInitConfig
       ? await NextApp.getKeycloakInitConfig(appContext)
-      : {};
+      : {}
   }
+
   async function getNextAppInitialProps(appContext){
     return NextApp.getInitialProps ? await NextApp.getInitialProps(appContext) : null;
   }
+
   class AppWithKeycloak extends React.Component {
     static async getInitialProps(appContext) {
-
-      const { isAuthenticated } = checkIfUserAuthenticated(appContext);
-      const [cmpInitialProps, keycloakInitConfig,pageProps] = await Promise.all([
+      const { isAuthenticated } = checkIfUserAuthenticated(appContext)
+      const [cmpInitialProps, keycloakInitConfig,nextAppProps] = await Promise.all([
         getComponentInitialProps(appContext),
         getKeycloakInitialProps(appContext),
         getNextAppInitialProps(appContext)
-      ]);
+      ])
 
       return {
         pageProps: {
           ...cmpInitialProps,
-          isAuthenticated,
-          ...pageProps
+          isAuthenticated
         },
+        nextAppProps,
         keycloakInitConfig
-      };
+      }
     }
 
     constructor(props) {
-      super(props);
+      super(props)
+
       this.state = {
-        isAuthenticated: props?.pageProps?.isAuthenticated ?? "null",
+        isAuthenticated: props?.pageProps?.isAuthenticated ?? 'false',
         keycloakInitConfig: {
           ...(providerProps?.initConfig ?? {}),
           ...(props?.keycloakInitConfig ?? {})
         }
-      };
+      }
     }
 
-    onEvent = async(event, error) => {
+    onEvent = (event, error) => {
       if (error) {
-        return console.error(error);
+        return console.error(error)
       }
 
-      if (event === "onAuthSuccess") {
-
+      if (event === 'onAuthSuccess') {
         this.setState({
-          isAuthenticated: "true"
-        });
+          isAuthenticated: 'true'
+        })
 
-        const user = await keycloak.loadUserInfo();
-        const expDate = keycloak.tokenParsed.exp * 1000;
-        setCookie("isAuthenticated", "true",expDate);
-        jsCookies.setItem("user", JSON.stringify(user), expDate, "/");
-         login(keycloak.token, expDate);
+        setCookie('isAuthenticated', 'true')
       }
 
-      if (event === "onAuthLogout") {
+      if (event === 'onAuthLogout') {
         this.setState({
-          isAuthenticated: "false"
-        });
+          isAuthenticated: 'false'
+        })
 
+        setCookie('isAuthenticated', 'false')
       }
 
-      if (event === "onReady") {
-        const isAuthenticated = keycloak.authenticated ? "true" : "false";
+      if (event === 'onReady') {
+        const isAuthenticated = keycloak.authenticated ? 'true' : 'false'
+
         this.setState({
           isAuthenticated
-        });
+        })
+
+        setCookie('isAuthenticated', isAuthenticated)
       }
 
       // Propagate events up
-      providerProps?.onEvent?.(event, error);
-    };
+      providerProps?.onEvent?.(event, error)
+    }
 
     isLoadingCheck = keycloak => {
-      const { pageProps } = this.props;
-      const { isAuthenticated } = this.state;
-      
-      const { isAuthenticated: isAuthProp } = pageProps;
+      const { pageProps } = this.props
+      const { isAuthenticated } = this.state
+
+      const { isAuthenticated: isAuthProp } = pageProps
 
       if (providerProps?.isLoadingCheck) {
         return providerProps.isLoadingCheck(
           keycloak,
           isAuthenticated || isAuthProp
-        );
+        )
       }
 
-      return false;
-    };
+      return false
+    }
 
     render() {
       // eslint-disable-next-line no-unused-vars
-      const { keycloakInitConfig: kcInitCfg, pageProps, ...props } = this.props;
-      const { isAuthenticated, keycloakInitConfig } = this.state;
+      const { keycloakInitConfig: kcInitCfg, pageProps, ...props } = this.props
+      const { isAuthenticated, keycloakInitConfig } = this.state
 
-      const { isAuthenticated: isAuthProp, ...childPageProps } = pageProps;
+      const { isAuthenticated: isAuthProp, ...childPageProps } = pageProps
 
       return (
         <ServerProvider isAuthenticated={isAuthenticated || isAuthProp}>
@@ -133,7 +129,7 @@ const appWithKeycloak = (
             <NextApp {...props} pageProps={childPageProps} />
           </KeycloakProvider>
         </ServerProvider>
-      );
+      )
     }
   }
 
@@ -142,11 +138,11 @@ const appWithKeycloak = (
       isAuthenticated: PropTypes.string.isRequired
     }).isRequired,
     keycloakInitConfig: PropTypes.shape({}).isRequired
-  };
+  }
 
   return hoistStatics(AppWithKeycloak, NextApp, {
     getInitialProps: true
-  });
-};
+  })
+}
 
-export default appWithKeycloak;
+export default appWithKeycloak
