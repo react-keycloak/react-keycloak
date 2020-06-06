59bcaea0adba45043236cfd160dc81bf9376f6e2
Mattia Panzeri <mattia.panzeri93@gmail.com>
2020-06-06 17:39:36 +0200

    feat: Add '@react-keycloak/ssr' package

---
+++ packages/ssr/src/internals/KeycloakStubProvider.js
@@ -0,0 +1,23 @@
+import * as React from 'react'
+import PropTypes from 'prop-types'
+
+import { KeycloakStubContext } from './context'
+
+class KeycloakStubProvider extends React.PureComponent {
+  render() {
+    const { children, keycloakStub } = this.props
+
+    return (
+      <KeycloakStubContext.Provider value={keycloakStub}>
+        {React.Children.only(children)}
+      </KeycloakStubContext.Provider>
+    )
+  }
+}
+
+KeycloakStubProvider.propTypes = {
+  children: PropTypes.element.isRequired,
+  keycloakStub: PropTypes.shape({}).isRequired,
+}
+
+export { KeycloakStubProvider }
