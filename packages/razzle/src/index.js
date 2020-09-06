477339a79a238fb862926c8e034e23fa54838df9
Mattia Panzeri <mattia.panzeri93@gmail.com>
2020-09-06 16:59:11 +0200

    breaking(razzle): remove 'razzle' module code

--- packages/razzle/src/index.js
+++
@@ -1,6 +0,0 @@
-export { default as SSRKeycloakProvider } from './SSRKeycloakProvider'
-export { default as withKeycloak } from './withKeycloak'
-export { default as useKeycloak } from './useKeycloak'
-
-export * as ClientPersistors from './persistors/client'
-export * as ServerPersistors from './persistors/server'
